41becb7a7afec436c758d009faf43630b97cbaaa
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-08-03 22:22:40 -0700

    swiper used in category and added to home page

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,56 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+
+import img1 from "../../../assets/home/slide1.jpg";
+import img2 from "../../../assets/home/slide2.jpg";
+import img3 from "../../../assets/home/slide3.jpg";
+import img4 from "../../../assets/home/slide4.jpg";
+import img5 from "../../../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <img src={img1} alt="" />
+        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+          Salads
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img2} alt="" />
+        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+          Pizza
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img3} alt="" />
+        <h3 className="text-4xl uppercase text-center text-white -mt-16">
+          Soup
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img4} alt="" />
+        <h3 className="text-4xl uppercase text-center text-white -mt-16">
+          Soup
+        </h3>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={img5} alt="" />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Category;
