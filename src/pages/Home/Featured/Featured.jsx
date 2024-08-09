0465f88830e6718311bed3c0154e9184a8c70426
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-08-08 23:03:15 -0700

    featured section added to home page

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,29 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import "./Featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured-item text-white pt-8 mt-20">
+      <SectionTitle subHeading={"Check It Out"} heading={"Featured Items"} />
+      <div className="md:flex justify-center items-center  pt-12 pb-20  px-36">
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div className="md:ml-10">
+          <p>December 11, 2025</p>
+          <p className="uppercase">Where I found this?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            impedit hic minus provident tempora ex consequatur ullam totam
+            excepturi. In, deserunt modi suscipit officiis voluptatum ab sed
+            vitae repudiandae voluptates?
+          </p>
+          <button className="btn btn-outline">Please Order </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
