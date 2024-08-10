9c65f9d00c433bfa9081fe98ed504f2a32547713
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-08-10 02:36:21 -0700

    different items loaded dynamically in menu page using MenuCategory.jsx

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,32 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Helmet>
         <title>Restaurant California | Our Menu</title>
       </Helmet>
       <Cover img={menuImg} title={"Our Menu"} />
+      <SectionTitle subHeading={"Dont Miss"} heading={"Todays Offer"} />
+      <MenuCategory items={offered} />
+      <MenuCategory items={dessert} title="Desserts" img={dessertImg} />
+      <MenuCategory items={pizza} title="Pizza" img={pizzaImg} />
+      <MenuCategory items={soup} title="Soup" img={soupImg} />
+      <MenuCategory items={salad} title="Salad" img={saladImg} />
     </div>
   );
 };
