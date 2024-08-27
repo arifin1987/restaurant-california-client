05f5361e974d95c10c303bcb1b9c3637fe22d40a
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-08-27 00:38:53 -0700

    cart items shown as a table format in MyCart.jsx

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -58,52 +58,6 @@ const FoodCard = ({ item }) => {
     }
   };
 
-  // const handleAddToCart = (item) => {
-  //   console.log(item);
-  //   if (user && user.email) {
-  //     const cartItem = {
-  //       menuItemId: _id,
-  //       name,
-  //       image,
-  //       price,
-  //       email: user.email,
-  //     };
-  //     fetch("http://localhost:5000/carts", {
-  //       method: "POST",
-  //       headers: {
-  //         "content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(cartItem),
-  //     })
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         if (data.insertedId) {
-  //           Swal.fire({
-  //             position: "top-end",
-  //             icon: "success",
-  //             title: "Order added in the cart",
-  //             showConfirmButton: false,
-  //             timer: 1500,
-  //           });
-  //         } else {
-  //           Swal.fire({
-  //             title: "Please login to order the food",
-
-  //             icon: "warning",
-  //             showCancelButton: true,
-  //             confirmButtonColor: "#3085d6",
-  //             cancelButtonColor: "#d33",
-  //             confirmButtonText: "Please Login",
-  //           }).then((result) => {
-  //             if (result.isConfirmed) {
-  //               navigate("/login", { state: { from: location } });
-  //             }
-  //           });
-  //         }
-  //       });
-  //   }
-  // };
-
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,9 +1,10 @@
 import { Helmet } from "react-helmet-async";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
   const [cart] = useCart();
-  console.log(cart);
+
   //   reduce method work on array element
   const total = cart.reduce((sum, item) => item.price + sum, 0);
   return (
@@ -11,8 +12,50 @@ const MyCart = () => {
       <Helmet>
         <title>Restaurant California | My Cart</title>
       </Helmet>
-      <h2 className="text-3xl">Total Items added:{cart?.length}</h2>
-      <h2 className="text-3xl">Total Price: ${total}</h2>
+      <div className="flex justify-between gap-12">
+        <h2 className="text-3xl">Total Items added:{cart?.length}</h2>
+        <h2 className="text-3xl">Total Price: ${total}</h2>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td className="text-end">${item.price}</td>
+                <td>
+                  <button className="btn btn-ghost btn-lg bg-green-600 text-white">
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
