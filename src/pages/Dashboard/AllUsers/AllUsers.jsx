ec4be7cda2814217c194b82334efe393f1a87abf
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-08-30 23:52:16 -0700

    made user as a admin and installed jsonwebtoken in the server side

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { refetch, data: users = [] } = useQuery({
@@ -10,8 +11,24 @@ const AllUsers = () => {
       return res.json();
     },
   });
-  const handleMakeAdmin = (id) => {
-    console.log(id);
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        refetch();
+
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
   };
   const handleDelete = (user) => {
     console.log(user);
@@ -46,7 +63,7 @@ const AllUsers = () => {
                     "admin"
                   ) : (
                     <button
-                      onClick={() => handleMakeAdmin(user._id)}
+                      onClick={() => handleMakeAdmin(user)}
                       className="btn btn-ghost btn-lg bg-blue-600 text-white"
                     >
                       <FaUserShield></FaUserShield>
